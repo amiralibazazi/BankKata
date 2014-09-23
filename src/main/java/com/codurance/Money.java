7234acd92b9326ed7096619938330bdd6d5d1466
Amir <amiralibazazi@gmail.com>
2014-09-23 13:58:03 +0100

    money now prints out it's value in the correct two decimal format

--- src/main/java/com/codurance/Money.java
+++ src/main/java/com/codurance/Money.java
@@ -1,11 +1,17 @@
 package com.codurance;
 
+import java.text.DecimalFormat;
+
 public class Money {
+
+    private final double value;
+    private final DecimalFormat TWO_DECIMAL_FORMAT = new DecimalFormat("#.00");
 
     public Money(double value) {
         if (value < 0) {
             throw new RuntimeException("Currency can not hold negative value");
         }
+        this.value = value;
     }
 
     public Money add(Money transactionAmount) {
@@ -13,6 +19,10 @@ public class Money {
     }
 
     public void printValue() {
+        System.out.print(toTwoDecimals(value));
+    }
 
+    private String toTwoDecimals(double value) {
+        return TWO_DECIMAL_FORMAT.format(value);
     }
 }
--- src/test/java/com/codurance/MoneyShould.java
+++ src/test/java/com/codurance/MoneyShould.java
@@ -1,13 +1,42 @@
 package com.codurance;
 
+import cucumber.api.java.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class MoneyShould {
 
     private static final double NEGATIVE_AMOUNT = -100.00;
+    private static final double ONE_HUNDRED = 100.00;
+    private ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+
+    @Before
+    public void initialise() {
+        System.setOut(new PrintStream(consoleOutput));
+    }
+
+    @After
+    public void breakDownStream() {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+    }
 
     @Test(expected=RuntimeException.class) public void
     throw_an_exception_when_handling_a_negative_value() {
         Money negativeMoney = new Money(NEGATIVE_AMOUNT);
+    }
+    
+    @Test public void
+    print_its_value() {
+        Money someMoney = new Money(ONE_HUNDRED);
+        someMoney.printValue();
+        assertThat(consoleOutput.toString(), is("100.00"));
     }
 }
